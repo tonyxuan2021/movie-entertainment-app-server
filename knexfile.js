b206b0ab3d245132fd51d840f5bc5b1859482668
tonyxuan2021 <tonyyexuan@gmail.com>
2022-06-28 14:06:10 -0700

    restructured knexfile to allow mysql access

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,32 @@
+// module.exports = {
+//   client: "mysql",
+//   connection: {
+//     host: "127.0.0.1",
+//     user: "root",
+//     password: "Yyh779955",
+//     database: "jwt_auth",
+//     charset: "utf8",
+//   },
+// };
 
-module.exports = {
-  client: "mysql",
-  connection: {
-    host: "127.0.0.1",
-    user: "root",
-    password: "Yyh779955",
-    database: "jwt_auth",
-    charset: "utf8",
+const connections = {
+  development: {
+    client: "mysql",
+    connection: {
+      host: "127.0.0.1",
+      user: "root",
+      password: "Yyh779955",
+      database: "jwt_auth",
+      charset: "utf8",
+    },
+  },
+  production: {
+    client: "mysql",
+    connection: process.env.JAWSDB_URL,
   },
 };
+
+module.exports =
+  process.env.NODE_ENV === "production"
+    ? connections.production
+    : connections.development;
